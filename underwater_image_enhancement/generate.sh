021be66f915edb86d5b922b07866203f0c47ce61
RaviShah1 <77947914+RaviShah1@users.noreply.github.com>
2021-12-20 16:14:51 -0600

    Create generate.sh

---
+++ underwater_image_enhancement/generate.sh
@@ -0,0 +1,5 @@
+python make_enhance_dataset.py \
+    -c ../input/tensorflow-great-barrier-reef/train.csv \
+    -i ../input/tensorflow-great-barrier-reef/train_images \
+    -o images/ \
+    -t clahe
